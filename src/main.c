eaed22fc86979d791dacc5cdb0d7fccb838d35aa
Sudipto Ghosh <sudipto@ghosh.pro>
2020-05-12 14:58:47 +0530

    initial commit

---
+++ src/main.c
@@ -0,0 +1,8 @@
+﻿#include "main.h"
+
+int main(void)
+{
+    // TODO
+
+    return 0;
+}
---
+++ src/main.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <time.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <applibs/log.h>
+#include <applibs/gpio.h>
+#include <hw/avnet_mt3620_sk.h>
+
+const struct timespec sleepTime = {1, 0};
+
+#endif
